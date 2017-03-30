9d24023c33adaaed111bf83cefff684e2ccb15ef
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-03-30 12:46:42 +0300

    OEHXCECF-59 / Clean code. PEP8

--- ckanext/extend_search/helpers.py
+++ ckanext/extend_search/helpers.py
@@ -1,25 +1,24 @@
 # -#-coding: utf-8 -#-
 import datetime
-import ckan.plugins.toolkit as tk
 import pytz
 import pylons.config as config
 from pytz import timezone
-from ckan.lib.helpers import date_str_to_datetime
-from ckan.common import _
-from ckan import model
+
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
 
 def extend_search_convert_local_to_utc_timestamp(str_timestamp):
     if not str_timestamp:
         return ''
 
-    local_datetime  = datetime.datetime.strptime(str_timestamp, DATETIME_FORMAT);
+    local_datetime = datetime.datetime.strptime(str_timestamp, DATETIME_FORMAT)
     tz_code = config.get('ckan.timezone', 'Australia/Melbourne')
     local = timezone(tz_code)
     utc_datetime = _make_aware(local_datetime, local)
     local_datetime = utc_datetime.astimezone(pytz.utc)
     return local_datetime.strftime(DATETIME_FORMAT)
+
 
 def _is_aware(value):
     """
@@ -30,6 +29,7 @@ def _is_aware(value):
     """
     return value.tzinfo is not None and value.tzinfo.utcoffset(value) is not None
 
+
 def _is_naive(value):
     """
     Determines if a given datetime.datetime is naive.
@@ -38,6 +38,7 @@ def _is_naive(value):
     http://docs.python.org/library/datetime.html#datetime.tzinfo
     """
     return value.tzinfo is None or value.tzinfo.utcoffset(value) is None
+
 
 def _make_aware(value, timezone):
     """
@@ -50,6 +51,7 @@ def _make_aware(value, timezone):
         # may be wrong around DST changes
         return value.replace(tzinfo=timezone)
 
+
 def _make_naive(value, timezone):
     """
     Makes an aware datetime.datetime naive in a given time zone.
@@ -59,4 +61,3 @@ def _make_naive(value, timezone):
         # available for pytz time zones
         value = timezone.normalize(value)
     return value.replace(tzinfo=None)
-
--- ckanext/extend_search/plugin_backup.py
+++ ckanext/extend_search/plugin_backup.py
@@ -2,11 +2,12 @@ import ckan
 import pylons
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.model import User, Package, Group
+from ckan.model import User, Package
 import ckan.model.meta as meta
 from ckan.lib.base import c
 from sqlalchemy import distinct
 from helpers import extend_search_convert_local_to_utc_timestamp
+
 
 class ExtendSearchPlugin(plugins.SingletonPlugin):
     '''
@@ -17,12 +18,10 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IPackageController, inherit=True)
 
-
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_resource('fanstatic', 'ckanext-datesearch')
         toolkit.add_resource('fanstatic', 'custodianpicker-module')
-
 
     # Add the custom parameters to Solr's facet queries
     def before_search(self, search_params):
@@ -32,8 +31,10 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
             # There are no extras in the search params, so do nothing.
             return search_params
 
-        start_date = extend_search_convert_local_to_utc_timestamp(extras.get('ext_startdate'))
-        end_date = extend_search_convert_local_to_utc_timestamp(extras.get('ext_enddate'))
+        start_date = extend_search_convert_local_to_utc_timestamp(
+            extras.get('ext_startdate'))
+        end_date = extend_search_convert_local_to_utc_timestamp(
+            extras.get('ext_enddate'))
         cust_id = extras.get('ext_cust_id')
 
         if not cust_id:
@@ -44,21 +45,20 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
         fq = search_params['fq']
 
         if start_date and end_date:
-            # Add a date-range query with the selected start and end dates into the
-            # Solr facet queries.
+            # Add a date-range query with the selected start and end dates into
+            # the Solr facet queries.
             fq = '{fq} +metadata_modified:[{start_date} TO {end_date}]'.format(
                 fq=fq, start_date=start_date, end_date=end_date)
 
-        #Add creator (user) id query to the Solr facet queries
+        # Add creator (user) id query to the Solr facet queries
         if cust_id:
             fq = '{fq} +maintainer:{cust_id}'.format(
                 fq=fq, cust_id=cust_id)
 
-        #return modified facet queries
+        # return modified facet queries
         search_params['fq'] = fq
 
         return search_params
-
 
     def after_search(self, search_params, search_results):
 
@@ -66,18 +66,19 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
                    'session': ckan.model.Session,
                    'user': pylons.c.user}
 
-        #set permission level: read (default is edit)
+        # set permission level: read (default is edit)
         data_dict = {'user': pylons.c.user, 'permission': 'read'}
-        #get list of organisations that the user is a member of
-        orgs = ckan.logic.get_action('organization_list_for_user')(context, data_dict)
+        # get list of organisations that the user is a member of
+        orgs = ckan.logic.get_action('organization_list_for_user')(
+            context, data_dict)
 
-        #user doesn't belong to an organisation
+        # user doesn't belong to an organisation
         if not orgs:
             print ('User is not a member of any organisations!')
             c.maintainers = []
             return search_params
 
-        #get a distinct list of members who belong to the organisations
+        # get a distinct list of members who belong to the organisations
         members = []
         for org in orgs:
             params = {'id': org['id'], 'object_type': 'user'}
@@ -87,24 +88,19 @@ class ExtendSearchPlugin(plugins.SingletonPlugin):
 
         memberset = set(members)
 
-
-        #need the user name to match with the maintainer field
-        current_user_name = None
+        # need the user name to match with the maintainer field
         member_names = []
         for member in memberset:
-            user = User.get(member[0])    #user id
+            user = User.get(member[0])    # user id
             member_names.append(user.name)
 
-
-        #get all maintainers
-        maintainers = [p[0] for p in meta.Session.query(distinct(Package.maintainer)) if p[0]]
+        # get all maintainers
+        maintainers = [p[0] for p in meta.Session.query(
+            distinct(Package.maintainer)) if p[0]]
         maintset = set(maintainers)
 
-
-        #filter maintainers by user-related organisation members
+        # filter maintainers by user-related organisation members
         results = maintset.intersection(member_names)
         c.maintainers = results
 
-
         return search_params
-
